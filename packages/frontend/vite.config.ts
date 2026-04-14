916593018bb6768597fbcfa4d9d6fee59cd570d0
Gerardo Rendon <gerardo.rendon@scisa.com.mx>
2026-04-14 10:36:26 -0600

    HMMM

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../../public',
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       '/api': `http://localhost:${process.env.VITE_API_PORT ?? 3001}`,
